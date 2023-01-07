301180e478109f015657320b87ea26d00857e475
Wypog <wyattdong1122@gmail.com>
2023-01-06 21:22:11 -0800

    Added "Zircon Ore", "Zircon Block", and "Deepslate Zircon Ore" to the Zircon Tutorial Tab.

--- src/main/java/net/wypog/tutorialmod/TutorialMod.java
+++ src/main/java/net/wypog/tutorialmod/TutorialMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.wypog.tutorialmod.block.ModBlocks;
 import net.wypog.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -22,6 +23,7 @@ public class TutorialMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
